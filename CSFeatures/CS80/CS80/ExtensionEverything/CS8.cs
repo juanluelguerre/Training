54ca957bab37ddad607be8b13cbf59cce132ce83
Juan Luis Guerrero Minero <jlguerrero@gmail.com>
2018-05-06 14:29:38 +0200

    Just created projects for 8.0 and examples for extensions everywhere !

---
+++ CSFeatures/CS80/CS80/ExtensionEverything/CS8.cs
@@ -0,0 +1,23 @@
+﻿//
+// https://blog.ndepend.com/c-8-0-features-glimpse-future/
+//
+using System;
+
+namespace ExtensionEverything
+{
+    public static class Program
+    {
+ 
+        
+        static void Main(string[] args)
+        {
+			int i = 0;
+
+			var even = i.IsEven(); 
+
+
+            Console.WriteLine("Press ENTER to finalize...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSFeatures/CS80/CS80/ExtensionEverything/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿//
+// https://blog.ndepend.com/c-8-0-features-glimpse-future/
+//
+using System;
+using System.Xml.Linq;
+
+namespace ExtensionEverything
+{
+	// 1) Static Class
+	public static class IntExtensions
+	{
+		// 2) Static method and "this" as first parm
+		public static bool IsEven(this int value)
+		{
+			return value % 2 == 0;
+		}
+	}
+
+	// New type declaration called an “extension”
+	//public extension Int2Extension extends int
+    //{
+    //    public bool IsEven => this % 2 == 0;
+    //}
+
+} 
