6b59371a0d6c03a1b56328e4a598c3c7f47389bc
Juan Luis Guerrero <jlguerrero@gmail.com>
2020-05-31 12:17:16 +0200

    Some CS8.0 samples Added !

---
+++ CSFeatures/CS80/ExploreCsharpEight/ExternalSystems.cs
@@ -0,0 +1,33 @@
+﻿//
+// Samples form:  https://github.com/dotnet/try-samples/tree/master/csharp8
+//
+
+namespace ConsumerVehicleRegistration
+{
+    public class Car
+    {
+        public int Passengers { get; set; }
+    }
+}
+
+namespace CommercialRegistration
+{
+    public class DeliveryTruck
+    {
+        public int GrossWeightClass { get; set; }
+    }
+}
+
+namespace LiveryRegistration
+{
+    public class Taxi
+    {
+        public int Fares { get; set; }
+    }
+
+    public class Bus
+    {
+        public int Capacity { get; set; }
+        public int Riders { get; set; }
+    }
+}
